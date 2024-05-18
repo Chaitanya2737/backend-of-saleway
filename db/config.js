2dce2d5a4988f64be1547302fe3cfea201db5943
Chaitanya <chaitanyasatarkar123@gmail.com>
2024-05-18 14:37:13 +0530

    mongoDb error

--- db/config.js
+++ db/config.js
@@ -1,9 +1,22 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://chaitanyasatarkar123:Chaitanya@cluster0.c6dlbcu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(()=>{
-    console.log("db connnected")
-}).catch((error) => {
-    console.log(error)
-}) // add your mongoDB collection with your connecting url
+const dbURI = "mongodb+srv://chaitanyasatarkar123:Chaitanya@cluster0.c6dlbcu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-module.exports =  mongoose.connection
+async function connectToDatabase() {
+    try {
+        await mongoose.connect(dbURI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            connectTimeoutMS: 100000, // 30 seconds
+            socketTimeoutMS: 450000, // 45 seconds
+        });
+        console.log("Database connected successfully");
+    } catch (error) {
+        console.error("Database connection error:", error);
+        // Optionally, you can add more specific error handling or retries here
+    }
+}
+
+connectToDatabase();
+
+module.exports = mongoose.connection;
